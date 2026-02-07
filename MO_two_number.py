799980ad01d31b310a2858e03de02cb04ca52b81
Jatin0399 <jgosain0399@gmail.com>
2026-02-08 01:48:45 +0530

    Update MO_two_number.py

--- MO_two_number.py
+++ MO_two_number.py
@@ -1,5 +1,10 @@
+#Perform Basic Mathematical Operations
+
+# Taking input from the user
 A = int(input('Enter the first number : '))
 B = int(input('Enter the second number : '))
+
+# Performing operations
 add = A + B
 print("Addition : ",add)
 
