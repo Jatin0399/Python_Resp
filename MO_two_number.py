ffd9b1fcb85116b2c2682c7be65fdf0fa1ec4ccf
Jatin0399 <jgosain0399@gmail.com>
2026-02-08 01:13:33 +0530

    Create MO_two_number.py

---
+++ MO_two_number.py
@@ -0,0 +1,13 @@
+A = int(input('Enter the first number : '))
+B = int(input('Enter the second number : '))
+add = A + B
+print("Addition : ",add)
+
+sub = A - B
+print("Subtraction : ",sub)
+
+mul = A * B
+print("Multiplication : ",mul)
+
+divi = A / B
+print("Division : ",divi)
